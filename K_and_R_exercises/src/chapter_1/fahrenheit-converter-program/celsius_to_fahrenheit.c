412e34f07cc52934b702f1f40df3c46429b840b7
johnnyflame <johnnyflamelee@gmail.com>
2018-04-12 10:14:35 +1200

    Began working on first chapter of K&R

---
+++ K_and_R_exercises/src/chapter_1/fahrenheit-converter-program/celsius_to_fahrenheit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *
+ * EX1.4 K&R
+ * Celsius to Fahrenheit converter
+ */
+
+
+int main(){
+    
+}
---
+++ K_and_R_exercises/src/chapter_1/fahrenheit-converter-program/farenheit_to_celsius.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+   for fahr = 0, 20, ..., 300 */
+
+void fahrenheit_to_celsius(){
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;   /* lower limite of temperature table */
+    upper = 300; /* upper limit */
+    step = 20;   /* step size */
+
+    fahr = lower;
+
+
+
+    printf("********************************************\n");
+    printf("**********FAHRENHEIT-CELSIUS-CONVERTER******\n");
+    printf("********************************************\n");
+
+    while (fahr <= upper){
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
+/**
+ * Exercise 1.4
+ * Changes celsius to Fahrenheit
+ * for celsius in range 1
+ */
+void celsius_to_fahrenheit(int lower, int upper,int increment){
+    int fahr, celsius;
+
+    for(celsius=lower; celsius <= upper; celsius+=increment)
+    {
+      fahr = (celsius * 9 / 5) + 32;
+      printf("%d\t%d\n", celsius, fahr);
+    }
+
+}
+
+
+int main()
+{
+    celsius_to_fahrenheit(0,100,1);
+    return 0;
+}
